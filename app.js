390288d075c8fe7e778d390e8c137445b02c1c67
jad Geitani <jadgeitani@outlook.com>
2022-08-12 14:26:50 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+const fs = require('fs');
+const pdf = require('pdf-parse');
+app.use(express.static("public"))
+ 
+let dataBuffer = fs.readFileSync('./CV.pdf');
+ 
+pdf(dataBuffer).then(function(data) {
+ 
+    
+    console.log(data.numpages);
+    console.log(data.numrender);
+   
+    console.log(data.info);
+ 
+    console.log(data.metadata); 
+    
+    console.log(data.version);
+   
+    console.log(data.text); 
+        
+});
+
+
+
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
